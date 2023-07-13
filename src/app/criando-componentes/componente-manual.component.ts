c9cb4f21ce9695a8bb8ca28c9127520cca237e9a
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-07-12 23:18:41 -0300

    Cria Componente Manualmente

---
+++ src/app/criando-componentes/componente-manual.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core'
+
+@Component({
+
+    selector: 'componente-manual-component',
+    template: `
+    <p>Componente criado manualmente com Angular 2</p>
+    `
+})
+export class ComponenteManualComponent{}
